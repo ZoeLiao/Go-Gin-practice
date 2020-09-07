2007394a5743b3c8804388fc00cd7b46c4a8f2b3
ZoeLiao <zoejoyuliao@gmail.com>
2020-09-07 23:46:24 +0800

    Add patch

--- apis/v1.go
+++ apis/v1.go
@@ -90,3 +90,61 @@ func GetURL(c *gin.Context) {
 		gin.H{"path": path, "url": URL.Url},
 	)
 }
+
+// Test example
+// @Description test
+// @name test name
+// @Accept  json
+// @Produce  json
+// @Param message body models.URL true "url"
+// @Success 200 {string} string	"ok"
+// @Router /shortener [patch]
+func UpdateURL(c *gin.Context) {
+	var URL models.URL
+	c.BindJSON(&URL)
+	path := URL.Path
+	url := URL.Url
+	notHas := global.GVA_DB.Where("path = ?", path).Find(&URL).RecordNotFound()
+	if !notHas {
+		global.GVA_LOG.Info("path: ", path, " URL not found.")
+		c.JSON(
+			http.StatusOK,
+			gin.H{"path": path, "res": "URL not found"},
+		)
+		return
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		global.GVA_LOG.Error(err)
+		c.JSON(
+			http.StatusOK,
+			gin.H{"message": err},
+		)
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		global.GVA_LOG.Error(err)
+		c.JSON(
+			http.StatusOK,
+			gin.H{"message": err},
+		)
+	} else {
+		err = global.GVA_DB.Update(&URL).Error
+		if err != nil {
+			global.GVA_LOG.Error(err)
+			c.JSON(
+				http.StatusOK,
+				gin.H{"url": url, "res": "Failed to update URL"},
+			)
+		} else {
+			c.JSON(
+				http.StatusOK,
+				gin.H{"url": url, "body": body},
+			)
+		}
+	}
+}
